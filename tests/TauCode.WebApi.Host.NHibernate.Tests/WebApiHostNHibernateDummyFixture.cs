c4967e54ae818f338e659948f8a14fdb8c82e17e
Andrey Kovalenko <ak@taucode.com>
2019-12-22 22:20:40 +0200

    Moving to nuget.org

---
+++ tests/TauCode.WebApi.Host.NHibernate.Tests/WebApiHostNHibernateDummyFixture.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TauCode.WebApi.Host.NHibernate.Tests
+{
+    [TestFixture]
+    public class WebApiHostNHibernateDummyFixture
+    {
+        [Test]
+        public void WebApiHostNHibernateDummyTest()
+        {
+            Assert.Pass("Passed for Jenkins.");
+        }
+    }
+}
